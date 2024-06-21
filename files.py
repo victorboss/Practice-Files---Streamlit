95a0c7654fb13ec201b6808821be167ef501be04
Debasish Bhattacharjee Victor <vviiccttoorr9898@gmail.com>
2024-06-21 18:31:36 +0600

    Add files via upload
    Files with Python Practice

---
+++ files.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.subheader("Uploading CSV files")
+df = st.file_uploader("Upload the CSV file:: ",type = ['csv','xlsx'])
+
+st.subheader('Loading the csv files')
+df = pd.read_csv('E:\Data Science\Practice Codes\Other Resources\Products.csv')
+
+if df is not None:
+    st.table(df.head())
+
+st.subheader('Working with Images')
+img_file = st.file_uploader("Upload the Image file:: ",type = ['png','jpeg'])
+
+if img_file is not None:
+    st.image(img_file)
+
+st.subheader('Working with Audio')
+audio_file = st.file_uploader("Upload the audio file:: ",type = ['wav','mp3'])
+if audio_file is not None:
+    st.audio(audio_file.read())
+
+st.subheader('Working with Video')
+video_file = st.file_uploader("Upload the video files:: ",type = ['mp4','mkv'])
+if video_file is not None:
+    st.video(video_file,start_time = 10)
